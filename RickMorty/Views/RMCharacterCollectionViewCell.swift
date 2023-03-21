b860b97ae3169c7ad20782b7adfe16a0c827e14b
BrianMwas <brnmwas@gmail.com>
2023-03-21 17:42:21 +0300

    Minor naming changes for project name initial

---
+++ RickMorty/Views/RMCharacterCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RMCharacterCollectionViewCell.swift
+//  RickMorty
+//
+//  Created by Brian on 21/03/2023.
+//
+
+import UIKit
+
+/// Single cell for a character
+final class RMCharacterCollectionViewCell: UICollectionViewCell {
+    
+}
