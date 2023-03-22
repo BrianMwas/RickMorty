23c77655f0bed5c4fb60cf7f9b28a75516e06ee2
BrianMwas <brnmwas@gmail.com>
2023-03-22 10:58:59 +0300

    Added character view cell view model

---
+++ RickMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  RMCharacterCollectionViewCellViewModel.swift
+//  RickMorty
+//
+//  Created by Brian on 21/03/2023.
+//
+
+import Foundation
+
+final class RMCharacterCollectionViewCellViewModel {
+    public let characterName: String
+    private let characterStatus: RMCharacterStatus
+    private let characterImageURL: URL?
+                
+    init(characterName: String, characterStatus: RMCharacterStatus, characterURL: URL?) {
+        self.characterImageURL =  characterURL
+        self.characterName = characterName
+        self.characterStatus = characterStatus
+    }
+    
+    public var characterStatusText: String {
+        return characterStatus.rawValue
+    }
+    
+    public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
+        // TODO: Abstract to image loader
+        guard let url = characterImageURL else {
+            completion(.failure(URLError(URLError.badURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
