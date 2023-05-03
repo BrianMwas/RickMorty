5eaf5005c205a3e37f248326d6e4f2394b529464
BrianMwas <brnmwas@gmail.com>
2023-05-03 20:57:42 +0300

    Updates changes

--- RickMorty/Views/Search/RMSearchBarView.swift
+++ RickMorty/Views/Search/RMSearchBarView.swift
@@ -9,12 +9,15 @@ import UIKit
 
 protocol RMSearchBarViewDelegate: AnyObject {
     func rmSearchInputView(_ inputView: RMSearchBarView, didSelectOption option: RMSearchBarViewViewModel.DynamicOption)
+    
+    func rmSearchInputView(_ inputView: RMSearchBarView, didSearchText text: String)
+    
+    func rmSearchInputDidTapSearchButton(_ inputView: RMSearchBarView)
 }
 
 final class RMSearchBarView: UIView {
-    
     weak var delegate: RMSearchBarViewDelegate?
-    
+
     private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.placeholder = "Search"
@@ -22,64 +25,65 @@ final class RMSearchBarView: UIView {
         return searchBar
     }()
 
-    
     private var viewModel: RMSearchBarViewViewModel? {
         didSet {
             guard let viewModel = viewModel, viewModel.hasDynamicOptions else {
                 return
             }
             let options = viewModel.options
-            createOptionsSelectionViews(options: options)
+            createOptionSelectionViews(options: options)
         }
     }
-    
+
     private var stackView: UIStackView?
-    
+
     // MARK: - Init
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
-        
-        backgroundColor = .systemBackground
         addSubviews(searchBar)
         addConstraints()
+        
+        searchBar.delegate = self
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError()
     }
-    
+
+    // MARK: - Private
+
     private func addConstraints() {
         NSLayoutConstraint.activate([
             searchBar.topAnchor.constraint(equalTo: topAnchor),
-            searchBar.rightAnchor.constraint(equalTo: rightAnchor),
             searchBar.leftAnchor.constraint(equalTo: leftAnchor),
-           
+            searchBar.rightAnchor.constraint(equalTo: rightAnchor),
+            searchBar.heightAnchor.constraint(equalToConstant: 58)
         ])
     }
-    
-    private func createOptionsStackView() -> UIStackView {
+
+    private func createOptionStackView() -> UIStackView {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
         stackView.spacing = 6
         stackView.distribution = .fillEqually
         stackView.alignment = .center
-        addSubviews(stackView)
-        
+        addSubview(stackView)
+
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
             stackView.leftAnchor.constraint(equalTo: leftAnchor),
             stackView.rightAnchor.constraint(equalTo: rightAnchor),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
         ])
-        
+
         return stackView
     }
-    
-    private func createOptionsSelectionViews(options: [RMSearchBarViewViewModel.DynamicOption]) {
-        
-        let stackView = createOptionsStackView()
+
+    private func createOptionSelectionViews(options: [RMSearchBarViewViewModel.DynamicOption]) {
+        let stackView = createOptionStackView()
         self.stackView = stackView
         for x in 0..<options.count {
             let option = options[x]
@@ -87,14 +91,12 @@ final class RMSearchBarView: UIView {
             stackView.addArrangedSubview(button)
         }
     }
-    
+
     private func createButton(
         with option: RMSearchBarViewViewModel.DynamicOption,
-        tag : Int
+        tag: Int
     ) -> UIButton {
         let button = UIButton()
-        
-        button.backgroundColor = .secondarySystemBackground
         button.setAttributedTitle(
             NSAttributedString(
                 string: option.rawValue,
@@ -105,43 +107,47 @@ final class RMSearchBarView: UIView {
             ),
             for: .normal
         )
-        
+        button.backgroundColor = .secondarySystemFill
         button.addTarget(self, action: #selector(didTapButton(_:)), for: .touchUpInside)
         button.tag = tag
         button.layer.cornerRadius = 6
+
         return button
     }
-    
+
     @objc
     private func didTapButton(_ sender: UIButton) {
         guard let options = viewModel?.options else {
             return
         }
         let tag = sender.tag
-        let selectedOption = options[tag]
-        delegate?.rmSearchInputView(self, didSelectOption: selectedOption)
+        let selected = options[tag]
+        delegate?.rmSearchInputView(self, didSelectOption: selected)
     }
-    
+
+    // MARK: - Public
+
     public func configure(with viewModel: RMSearchBarViewViewModel) {
         searchBar.placeholder = viewModel.searchPlaceholderText
         self.viewModel = viewModel
     }
-    
+
     public func presentKeyboard() {
         searchBar.becomeFirstResponder()
     }
-    
-    public func update(option: RMSearchBarViewViewModel.DynamicOption, value: String) {
-        print("We got here")
+
+    public func update(
+        option: RMSearchBarViewViewModel.DynamicOption,
+        value: String
+    ) {
         // Update options
         guard let buttons = stackView?.arrangedSubviews as? [UIButton],
               let allOptions = viewModel?.options,
               let index = allOptions.firstIndex(of: option) else {
             return
         }
-        
-        let button: UIButton = buttons[index]
-        button.setAttributedTitle(
+
+        buttons[index].setAttributedTitle(
             NSAttributedString(
                 string: value.uppercased(),
                 attributes: [
@@ -153,3 +159,18 @@ final class RMSearchBarView: UIView {
         )
     }
 }
+
+extension RMSearchBarView: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // Notify delegate of the change
+        delegate?.rmSearchInputView(self, didSearchText: searchText)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Notify that the delegate was updated
+        // Get rid of the keyboard
+        searchBar.resignFirstResponder()
+        delegate?.rmSearchInputDidTapSearchButton(self)
+    }
+}
+
--- RickMorty/Views/Search/RMSearchView.swift
+++ RickMorty/Views/Search/RMSearchView.swift
@@ -14,79 +14,106 @@ protocol RMSearchViewDelegate: AnyObject {
 final class RMSearchView: UIView {
     
     weak var delegate: RMSearchViewDelegate?
-    private let viewModel: RMSearchViewViewModel
     
+    private let viewModel: RMSearchViewViewModel
     
     // MARK: - Subviews
     
-    // Search bar, selection
-    private let inputBarView = RMSearchBarView()
+    private let searchInputView = RMSearchBarView()
     
-    // No results views
     private let noResultsView = RMNoSearchResultsView()
     
-    // Results collection view
+    private let resultsView = RMSearchResultsView()
+    
+    // Results collectionView
     
     // MARK: - Init
-    init(frame: CGRect, viewModel: RMSearchViewViewModel) {
-        self.viewModel = viewModel
+    
+    init(frame: CGRect, vm: RMSearchViewViewModel) {
+        self.viewModel = vm
         super.init(frame: frame)
         backgroundColor = .systemBackground
         translatesAutoresizingMaskIntoConstraints = false
-        addSubviews(noResultsView, inputBarView)
+        addSubviews(noResultsView, searchInputView, resultsView)
         addConstraints()
         
-        inputBarView.configure(with: .init(type: viewModel.config.type))
-        inputBarView.delegate = self
+        searchInputView.configure(with: RMSearchBarViewViewModel(type: viewModel.config.type))
+        searchInputView.delegate = self
         
-        viewModel.registerOptionChangeBlock { tuple in
-            print(String(describing: tuple))
-//            self.inputBarView.update(option: tuple.0, value: tuple.1)
-        }
+        setupHandlers(viewModel: viewModel)
     }
     
-    required init?(coder: NSCoder?) {
+    required init?(coder: NSCoder) {
         fatalError()
     }
     
+    private func setupHandlers(viewModel: RMSearchViewViewModel) {
+        viewModel.registerOptionChangeBlock { tuple in
+            self.searchInputView.update(option: tuple.0, value: tuple.1)
+        }
+        
+        viewModel.registerSearchResultsHandler { [weak self] results in
+            DispatchQueue.main.async {
+                self?.resultsView.configure(with: results)
+                self?.noResultsView.isHidden = true
+                self?.resultsView.isHidden = false
+            }
+    
+        }
+        
+        viewModel.registerNoResultsHandler { [weak self] in
+            DispatchQueue.main.async {
+                self?.noResultsView.isHidden = false
+                self?.resultsView.isHidden = true
+            }
+        }
+    }
+
     private func addConstraints() {
         NSLayoutConstraint.activate([
-            // Search view
-            inputBarView.topAnchor.constraint(equalTo: topAnchor),
-            inputBarView.leftAnchor.constraint(equalTo: leftAnchor),
-            inputBarView.rightAnchor.constraint(equalTo: rightAnchor),
-            inputBarView.heightAnchor.constraint(equalToConstant: viewModel.config.type == .episode ? 55 : 110),
-            
-            // No results view
-            noResultsView.heightAnchor.constraint(equalToConstant: 150),
+            // Search input view
+            searchInputView.topAnchor.constraint(equalTo: topAnchor),
+            searchInputView.leftAnchor.constraint(equalTo: leftAnchor),
+            searchInputView.rightAnchor.constraint(equalTo: rightAnchor),
+            searchInputView.heightAnchor.constraint(equalToConstant: viewModel.config.type == .episode ? 55 : 110),
+
+            // No results
             noResultsView.widthAnchor.constraint(equalToConstant: 150),
+            noResultsView.heightAnchor.constraint(equalToConstant: 150),
             noResultsView.centerXAnchor.constraint(equalTo: centerXAnchor),
             noResultsView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            resultsView.topAnchor.constraint(equalTo: searchInputView.bottomAnchor),
+            resultsView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            resultsView.leftAnchor.constraint(equalTo: leftAnchor),
+            resultsView.rightAnchor.constraint(equalTo: rightAnchor),
+            
         ])
     }
-    
+
     public func presentKeyboard() {
-        inputBarView.presentKeyboard()
+        searchInputView.presentKeyboard()
     }
 }
 
 // MARK: - CollectionView
+
 extension RMSearchView: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 0
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         return cell
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
-        
+
+
     }
 }
-
 
 // MARK: - RMSearchBarViewDelegate
 extension RMSearchView: RMSearchBarViewDelegate {
@@ -96,4 +123,13 @@ extension RMSearchView: RMSearchBarViewDelegate {
     ) {
         delegate?.rmSearchView(self, didSelectOption: option)
     }
+    
+    func rmSearchInputView(_ inputView: RMSearchBarView, didSearchText text: String) {
+        viewModel.set(query: text)
+    }
+    
+    func rmSearchInputDidTapSearchButton(_ inputView: RMSearchBarView) {
+        viewModel.executeSearch()
+    }
 }
+
